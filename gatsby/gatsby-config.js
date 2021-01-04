a4764acf1f5f6d85c928436538c86cce3733b3da
Trung Tran <trungtran.chunml@gmail.com>
2021-01-04 11:24:52 +0900

    Configure gatsby-source-sanity plugin

---
+++ gatsby/gatsby-config.js
@@ -0,0 +1,20 @@
+import { config } from "dotenv";
+
+config({ path: "dev.env" });
+
+export const siteMetadata = {
+  title: "Tokyo Eats",
+  siteUrl: "https://tokyo.eats",
+  description: "Tokyo Eats for Japan",
+};
+export const plugins = [
+  {
+    resolve: "gatsby-source-sanity",
+    options: {
+      projectId: "ugouospp",
+      dataset: "production",
+      watchMode: true,
+      token: process.env.SANITY_TOKEN,
+    },
+  },
+];
