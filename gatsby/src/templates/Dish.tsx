d6d9379664570dd9790f11f78937145e38c61dda
Trung Tran <trungtran.chunml@gmail.com>
2021-01-05 15:49:16 +0900

    Change Dish page layout

--- gatsby/src/templates/Dish.tsx
+++ gatsby/src/templates/Dish.tsx
@@ -2,6 +2,7 @@ import { graphql, Link } from "gatsby";
 import React, { useContext, useState } from "react";
 import Img from "gatsby-image";
 import { FaPlus, FaMinus } from "react-icons/fa";
+import { navigate } from "@reach/router";
 import { AllStoreType, DishType } from "../utils/types";
 import Banner from "../components/Banner";
 import formatMoney from "../utils/formatMoney";
@@ -25,25 +26,14 @@ const Dish: React.FC<DishProps> = ({ data }) => {
       <Banner text={store.nodes[0].name} />
       <div className="grid grid-cols-2 gap-x-12 items-center my-6 border-solid border-2 border-gray-2 rounded-xl p-5 shadow-2xl">
         <Link
-          className="col-span-2 mb-5"
+          className="col-span-2 mb-6"
           to={`/store/${store.nodes[0].slug.current}`}
         >
           <p className="text-center border-solid border-2 border-yellow-400 rounded-2xl mx-auto w-2/5 py-2 hover:bg-yellow-400 hover:text-white">
             Back to store
           </p>
         </Link>
-        <div className="text-center">
-          <Img fluid={dish.image.asset.fluid} />
-          <h2 className="text-xl font-medium mb-4 bg-yellow-400 transform -rotate-2 -translate-y-5">
-            {dish.name}
-          </h2>
-        </div>
-        <div className="text-center">
-          <p>{dish.description}</p>
-          <hr className="my-4" />
-          <p className="bg-yellow-400">Price: ¥{dish.price}</p>
-        </div>
-        <form className="col-span-2 my-8 mx-4 flex justify-center leading-none">
+        <form className="col-span-2 mb-6 mx-4 flex justify-center leading-none">
           <button
             type="button"
             className="text-white bg-yellow-400 p-6 rounded-full"
@@ -60,10 +50,12 @@ const Dish: React.FC<DishProps> = ({ data }) => {
             <FaMinus />
           </button>
           <button
-            type="submit"
+            type="button"
             className="text-white bg-red-400 p-6 rounded-2xl flex-grow mx-8"
+            disabled={!(order[dish.id] > 0)}
+            onClick={() => navigate("/order")}
           >
-            Order
+            Confirm Order
           </button>
           <button
             type="button"
@@ -82,29 +74,43 @@ const Dish: React.FC<DishProps> = ({ data }) => {
           </button>
         </form>
         {order[dish.id] > 0 && (
-          <div className="col-span-2 grid grid-cols-3 my-4 text-sm text-center mx-auto w-3/5">
-            <p className="mr-auto">{formatMoney(dish.price)}</p>
-            <p>x</p>
-            <p className="ml-auto">{order[dish.id]}</p>
-            <hr className="col-span-3 my-4" />
-            <div />
-            <div />
-            <p className="ml-auto">
-              {formatMoney(dish.price * order[dish.id])}
-            </p>
-            <div />
-            <p>VAT</p>
-            <p className="ml-auto">
-              {formatMoney(dish.price * order[dish.id] * 0.08)}
-            </p>
-            <hr className="col-span-3 my-4" />
-            <div />
-            <p>Total</p>
-            <p className="ml-auto">
-              {formatMoney(dish.price * order[dish.id] * 1.08)}
-            </p>
-          </div>
+          <>
+            <div className="col-span-2 grid grid-cols-3 mb-6 text-sm text-center mx-auto w-3/5">
+              <p className="mr-auto">{formatMoney(dish.price)}</p>
+              <p>x</p>
+              <p className="ml-auto">{order[dish.id]}</p>
+              <hr className="col-span-3 my-4" />
+              <div />
+              <div />
+              <p className="ml-auto">
+                {formatMoney(dish.price * order[dish.id])}
+              </p>
+              <div />
+              <p>VAT</p>
+              <p className="ml-auto">
+                {formatMoney(dish.price * order[dish.id] * 0.08)}
+              </p>
+              <hr className="col-span-3 my-4" />
+              <div />
+              <p>Total</p>
+              <p className="ml-auto">
+                {formatMoney(dish.price * order[dish.id] * 1.08)}
+              </p>
+            </div>
+            <hr className="col-span-2 mb-6" />
+          </>
         )}
+        <div className="text-center">
+          <Img fluid={dish.image.asset.fluid} />
+          <h2 className="text-xl font-medium mb-4 bg-yellow-400 transform -rotate-2 -translate-y-5">
+            {dish.name}
+          </h2>
+        </div>
+        <div className="text-center">
+          <p>{dish.description}</p>
+          <hr className="my-4" />
+          <p className="bg-yellow-400">Price: ¥{dish.price}</p>
+        </div>
       </div>
     </>
   );
